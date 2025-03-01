1adcdf379454ba4f6d0fc0dbf3fcb6452898cb3b
navneetco0 <navneetharsh3@gmail.com>
2025-03-01 22:55:30 +0530

    [Fix]: port fixed

--- server/dist/index.js
+++ server/dist/index.js
@@ -6,7 +6,17 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const configs_1 = require("./configs");
 const node_cron_1 = __importDefault(require("node-cron"));
 const controllers_1 = require("./controllers");
+const express_1 = __importDefault(require("express"));
+const app = (0, express_1.default)();
+const PORT = process.env.PORT || 3000;
 (0, configs_1.connectToDatabase)();
 node_cron_1.default.schedule("*/5 * * * *", controllers_1.checkForChanges);
 console.log("⏳ Monitoring started. Checking for updates every 5 seconds...");
+app.get("/", (req, res) => {
+    res.send("Cron job is running...");
+});
+// Start the server
+app.listen(PORT, () => {
+    console.log(`✅ Server running on port ${PORT}`);
+});
 //# sourceMappingURL=index.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,23 @@
 import { connectToDatabase } from "./configs";
 import cron from "node-cron";
 import { checkForChanges } from "./controllers";
+import express from "express";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 connectToDatabase();
 
 cron.schedule("*/5 * * * *", checkForChanges);
 
 console.log("⏳ Monitoring started. Checking for updates every 5 seconds...");
+
+app.get("/", (req, res) => {
+    res.send("Cron job is running...");
+});
+  
+
+// Start the server
+app.listen(PORT, () => {
+    console.log(`✅ Server running on port ${PORT}`);
+});
